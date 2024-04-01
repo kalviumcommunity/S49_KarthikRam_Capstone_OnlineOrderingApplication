18797cef43abfd70949fca16908a266c55dafcba
KarthikRam14 <karthikram1441@gmail.com>
2024-04-01 15:48:01 +0530

    created post endpoints

--- Backend/BagRoute.js
+++ Backend/BagRoute.js
@@ -6,7 +6,18 @@ bagRoute.get("/selected", (req, res)=>{
         console.log("request recieved on /bag/selected")
         res.json({message: "Bag data"})
     }catch(err){
-        console.log("Internal server error", err)
+        console.error("Internal server error", err)
+        res.status(500).json({error: "Internal server error"})
+    }
+})
+
+bagRoute.post('/selected', (req, res)=>{
+    try{
+        console.log("Post received on /bag/selected")
+        const newData = req.body
+        res.json({message: "Data sucessfully posted", data: newData})
+    }catch(err){
+        res.status(500).json({error: "Internal server error"})
     }
 })
 
