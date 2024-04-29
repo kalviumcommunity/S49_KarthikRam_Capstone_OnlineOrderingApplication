d009643ce054229acf64591a85af27d8f988f2a0
KarthikRam14 <karthikram1441@gmail.com>
2024-04-29 17:14:30 +0530

    completed 35% of homepage

--- Backend/Models/HomepageModel.js
+++ Backend/Models/HomepageModel.js
@@ -1,8 +1,11 @@
 const mongoose = require('mongoose')
 
 const HomepageSchema = mongoose.Schema({
-    name: String,
-    img: String
+    name : String,
+    img: String,
+    price: Number,
+    available: String,
+    category: String
 })
 
 const HomepageModel = mongoose.model('homepagedata', HomepageSchema)
