c95140164dc11f66574a0e2f9b986156d7d6aa2e
KarthikRam14 <karthikram1441@gmail.com>
2024-06-18 23:22:19 +0530

    created sign-in page and login page. left with login page

--- Frontend/ordering-application/src/App.jsx
+++ Frontend/ordering-application/src/App.jsx
@@ -3,7 +3,9 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import "./App.css";
 import HomePage from "./Components/HomePage";
 import Bag from "./Components/Bag"; 
-import Account from "./Components/Account"; 
+import Account from "./Components/Account";
+import SignIn from "./Components/SignIn"; 
+import LoginIn from "./Components/LoginIn";
 // import Navbar from "./Components/Navbar";
 
 function App() {
@@ -13,6 +15,8 @@ function App() {
         <Route path="/bag" element={<Bag />} /> 
         <Route path="/account" element={<Account />} /> 
         <Route path="/" element={<HomePage />} /> 
+        <Route path="/sign-in" element={<SignIn/>}/>
+        <Route path="/login" element={<LoginIn/>}/>
       </Routes>
     </Router>
   );
