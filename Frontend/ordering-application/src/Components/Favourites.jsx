2c76b7c229afd1bba3eb8982220f7056583aa179
KarthikRam14 <karthikram1441@gmail.com>
2024-07-03 23:47:12 +0530

    Completed Favourites page with building and designing

--- Frontend/ordering-application/src/Components/Favourites.jsx
+++ Frontend/ordering-application/src/Components/Favourites.jsx
@@ -1,8 +1,32 @@
-import React from 'react'
+import { MdKeyboardArrowLeft } from "react-icons/md";
+import { useNavigate } from 'react-router-dom';
+import {allItems} from './HomePage'
+import { MdCurrencyRupee } from "react-icons/md";
+import { IoStarSharp } from "react-icons/io5";
 
 function Favourites() {
+
+  const Navigate = useNavigate()
+
+  const handleBack = () =>{
+    Navigate('/account')
+  }
   return (
-    <div>Favourites</div>
+    <div style={{margin: '2vw 6vw'}}>
+      <h1 className='flex' style={{alignItems: 'center'}}><MdKeyboardArrowLeft className='back-icon' onClick={handleBack}/>Your Favourites</h1>
+      <div className="fav-grid" style={{margin: '3vw 4vw'}}>
+        <div className="fav-item">
+          <div className="fav-item-image-div"><img src={allItems.chapathi} alt="chapathi" className="fav-item-image"/></div>
+          <div className="flex fav-item-div-two">
+            <div>
+              <p className="fav-item-name">Chapathi</p>
+              <p className="flex fav-item-price"><MdCurrencyRupee/>40</p>
+            </div>
+            <div><IoStarSharp className="fav-icon"/></div>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
